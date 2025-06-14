9883e8abf71298dba501592d5d876e34948a3337
7egz <mustafareda3210@gmail.com>
2025-06-14 15:28:23 +0300

    Finished Authors Module

---
+++ Bookify.Web/Core/ViewModels/Author/AuthorFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Web.Core.ViewModels.Author
+{
+    public class AuthorFormViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Author name is required.")]
+        [Remote("IsAllowed", "Authors", AdditionalFields = "Id", ErrorMessage = "An Author with the same name already exists.")]
+        public string Name { get; set; } = null!;
+    }
+}
